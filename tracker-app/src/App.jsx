a01572021a0785b6d30ab55223c16bb49f38ca54
antoineedwards <edwardsantoine333@gmail.com>
2024-08-01 23:52:27 -0500

    11:52 thur

--- tracker-app/src/App.jsx
+++ tracker-app/src/App.jsx
@@ -1,19 +1,38 @@
+'use client'
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar } from '@mui/material'
+import { Box, Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar } from '@mui/material'
 import { PhotoCamera } from '@mui/icons-material'
-const App = () => {
+import { makeStyles } from '@mui/material/styles'
+import { useState, useEffect } from 'react';
+import { firestore } from '/firebase';
+
+function App() {
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemname, setItemName] = useState('')
+
+const updateInventory = async () => {
+  const snapshot = query(collection(firestore, 'inventory'))
+  const docs = await getDocs(snapshot)
+  docs.forEach((doc)=>{
+   inventoryList.push({
+    name: doc.id,
+    ...doc.data(),
+  })
+  setInventory(inventoryList)
+})
+}
+
+useEffect(() => {
+  updateInventory()
+}, [])
   return (
-    <div>
-      <>
-        <CssBaseline />
-        <AppBar position='relative' color='default'>
-          <Toolbar>
-            <PhotoCamera />
-            <Typography variant='h6'>Antoine Edwards</Typography>
-          </Toolbar>
-        </AppBar>
-      </>
-    </div>
+    <Box>
+      <Typography variant='h1'>Inventory Tracker</Typography>
+      {
+        inventory.forEach(item) 
+      }
+    </Box>
   )
 }
 
--- tracker-app/src/firebase.js
+++ tracker-app/src/firebase.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/App";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore} from "firebase/Firestore";
+import App from './tracker-app/src/App';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +24,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 
-export {firestore}
+export {app, firestore}
